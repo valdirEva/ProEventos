44c71b7f2480c8ae45c78350268d92e3262123e7
valdirEva <valdirevaristo.silva@gmail.com>
2022-03-08 10:17:01 -0300

    Primeiro commit

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento[]{
+            new Evento(){
+                EventoId = 1,
+                Tema = "Teste1",
+                Local = "SJC",
+                Lote = "1º lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                ImagemUrl = "fot.png"
+            },
+             new Evento(){
+                EventoId = 2,
+                Tema = "Teste2",
+                Local = "SJC",
+                Lote = "1º lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                ImagemUrl = "fot.png"
+            },
+        };
+        public EventoController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo de post";
+        }
+
+        [HttpPut]
+        public string Put(int id)
+        {
+            return $"Exemplo de post id = {id}";
+        }
+
+         [HttpDelete]
+        public string Delete(int id)
+        {
+            return $"Exemplo de delete id = {id}";
+        }
+    }
+}
